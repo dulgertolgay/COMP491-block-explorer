004429d984113b7460809760f2f1eabf1578accf
Tolgay Dülger <49791793+dulgertolgay@users.noreply.github.com>
2023-06-02 11:26:13 +0300

    image fix

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "";
-let basePath = "";
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// let assetPrefix = "";
+// let basePath = "";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // assetPrefix: assetPrefix,
+  // basePath: basePath,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
